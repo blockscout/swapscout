ca886e90f0d4b4b1521e61349b0341bd0121443e
Max Alekseenko <nnwen0609@gmail.com>
2024-10-14 17:24:28 +0200

    update banner width

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,7 +5,7 @@ export const Banner: React.FC<{ text?: string }> = ({ text }) => {
   if (!text) return null;
 
   return (
-    <div className="flex justify-center items-center text-center w-fit rounded px-2 py-0.5 mb-6 bg-[#c6f6d5] text-[#2d3748]">
+    <div className="flex justify-center items-center text-center w-fit rounded px-2 py-0.5 mb-6 bg-[#c6f6d5] text-[#2d3748] max-w-[430px]">
       <ReactMarkdown
         components={{
           a: ({ node, ...props }) => (
