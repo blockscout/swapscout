c1aa0e81098af46cf4ad64cbd3670b1ec9f30072
Max Alekseenko <nnwen0609@gmail.com>
2024-10-14 17:04:26 +0200

    enable more chains

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -6,6 +6,26 @@ import { LiFiWidget, WidgetSkeleton } from '@lifi/widget';
 import { useWalletContext } from '@/components/WalletProvider';
 import { ClientOnly } from './ClientOnly';
 
+const explorerUrls = {
+  1: ['https://eth.blockscout.com/'],
+  10: ['https://optimism.blockscout.com/'],
+  30: ['https://rootstock.blockscout.com/'],
+  100: ['https://gnosis.blockscout.com/'],
+  122: ['https://explorer.fuse.io/'],
+  137: ['https://polygon.blockscout.com/'],
+  324: ['https://zksync.blockscout.com/'],
+  1088: ['https://andromeda-explorer.metis.io/'],
+  1101: ['https://zkevm.blockscout.com/'],
+  5000: ['https://explorer.mantle.xyz/'],
+  8453: ['https://base.blockscout.com/'],
+  13371: ['https://explorer.immutable.com/'],
+  34443: ['https://explorer.mode.network/'],
+  42161: ['https://arbitrum.blockscout.com/'],
+  42220: ['https://explorer.celo.org/mainnet/'],
+  81457: ['https://blast.blockscout.com/'],
+  1313161554: ['https://explorer.mainnet.aurora.dev/'],
+};
+
 export function Widget() {
   const { chainId } = useWalletContext();
 
@@ -31,18 +51,9 @@ export function Widget() {
     },
     fromChain: chainId || 1,
     fromToken: '0x0000000000000000000000000000000000000000',
-    explorerUrls: {
-      1: ['https://eth.blockscout.com/'],
-      10: ['https://optimism.blockscout.com/'],
-      100: ['https://gnosis.blockscout.com/'],
-      137: ['https://polygon.blockscout.com/'],
-      324: ['https://zksync.blockscout.com/'],
-      1101: ['https://zkevm.blockscout.com/'],
-      8453: ['https://base.blockscout.com/'],
-      42161: ['https://arbitrum.blockscout.com/'],
-    },
+    explorerUrls,
     chains: {
-      allow: [1, 10, 100, 137, 324, 1101, 8453, 42161],
+      allow: Object.keys(explorerUrls).map(Number),
     }
   } as Partial<WidgetConfig>;
 
