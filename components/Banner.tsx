abfa1b6c579542fb096090f69746d1224e5e77b1
Max Alekseenko <nnwen0609@gmail.com>
2024-11-08 14:01:52 +0100

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -6,7 +6,7 @@ export const Banner: React.FC<{ text?: string }> = ({ text }) => {
   if (!text) return null;
 
   return (
-    <div className="flex justify-center items-center text-center w-fit rounded px-2 py-0.5 mb-6 bg-[#c6f6d5] text-[#2d3748] max-w-[430px]">
+    <div className="w-full flex justify-center items-center text-center w-fit rounded-lg p-2 mb-6 bg-[#c6f6d5] text-[#2d3748] max-w-[430px]">
       <ReactMarkdown
         rehypePlugins={[rehypeRaw]}
         components={{
