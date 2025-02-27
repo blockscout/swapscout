d081d6791b26dcd95b05dc61bbcfa2bd493e9fd8
maikReal <krupin.mihailw@gmail.com>
2025-02-27 11:49:54 +0000

    disabled email and social logins

--- contexts/WagmiContextProvider.tsx
+++ contexts/WagmiContextProvider.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createAppKit } from '@reown/appkit/react';
-import React, { type ReactNode } from 'react';
-import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createAppKit } from "@reown/appkit/react";
+import React, { type ReactNode } from "react";
+import { cookieToInitialState, WagmiProvider, type Config } from "wagmi";
 
-import { wagmiAdapter, projectId } from '@/config/wagmi';
-import { networks } from '@/networks';
+import { wagmiAdapter, projectId } from "@/config/wagmi";
+import { networks } from "@/networks";
 
 // Set up queryClient
 const queryClient = new QueryClient();
 
 if (!projectId) {
-  throw new Error('Project ID is not defined');
+  throw new Error("Project ID is not defined");
 }
 
 // Set up metadata
 const metadata = {
-  name: 'Swapscout',
-  description: 'Swap between chains fast and easy. Powered by Blockscout',
-  url: 'https://swap.blockscout.com/',
-  icons: ['https://swap.blockscout.com/favicon-256x256.png'],
-}
+  name: "Swapscout",
+  description: "Swap between chains fast and easy. Powered by Blockscout",
+  url: "https://swap.blockscout.com/",
+  icons: ["https://swap.blockscout.com/favicon-256x256.png"],
+};
 
 // Create the modal
 createAppKit({
@@ -31,17 +31,31 @@ createAppKit({
   metadata: metadata,
   features: {
     analytics: true,
+    socials: false,
+    email: false,
     swaps: false,
     onramp: false,
   },
-  themeMode: 'light',
+  themeMode: "light",
 });
 
-function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
-  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies);
+function ContextProvider({
+  children,
+  cookies,
+}: {
+  children: ReactNode;
+  cookies: string | null;
+}) {
+  const initialState = cookieToInitialState(
+    wagmiAdapter.wagmiConfig as Config,
+    cookies
+  );
 
   return (
-    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+    <WagmiProvider
+      config={wagmiAdapter.wagmiConfig as Config}
+      initialState={initialState}
+    >
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
