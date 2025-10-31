f4680a555a4430e455d2625f187346e3a16474a2
Max Alekseenko <nnwen0609@gmail.com>
2025-10-31 15:27:18 +0100

    add utm tags

--- components/Stub.tsx
+++ components/Stub.tsx
@@ -24,7 +24,7 @@ export function Stub() {
         </p>
 
         <a
-          href="https://eth.blockscout.com/essential-dapps/swap"
+          href="https://eth.blockscout.com/essential-dapps/swap?utm_source=swapscout&utm_medium=deprecation-banner"
           className="mt-8 inline-flex items-center justify-center rounded-lg bg-[#5459D6] px-3 py-2 text-base font-semibold text-white transition-opacity hover:opacity-[0.9]"
         >
           Access Swap dapp
